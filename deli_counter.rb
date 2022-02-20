ee1f356b4838f56262893a7b906bbb343f4455e0
Julie Evans <juliemevans17@gmail.com>
2022-02-19 20:28:55 -0600

    commit tree 97544028756c57fafd8402ab7c0dfb6241da8a5c

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,29 @@
-# Write your code here.
+katz_deli = []
+
+def line katz_deli
+  counter = 0
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  else
+    customer_in_line = katz_deli.map do |customer|
+      counter = counter + 1
+      "#{counter}. #{customer}"
+    end
+    puts "The line is currently: #{customer_in_line.join(" ")}"
+  end
+end
+
+def take_a_number(katz_deli, new_customer_name)
+  new_number = katz_deli.length + 1
+  puts "Welcome, #{new_customer_name}. You are number #{new_number} in line."
+  katz_deli << new_customer_name
+end
+
+def now_serving katz_deli
+  if katz_deli.length == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{katz_deli[0]}."
+    katz_deli.shift
+  end
+end
